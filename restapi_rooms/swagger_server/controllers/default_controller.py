9c08a4bd793ec1988dc23b70758441ab952c0d65
TarikTornes <tarik.tornes03@gmail.com>
2025-01-04 17:54:11 +0100

    Added framework for extended swagger server with booking capabilities

---
+++ restapi_rooms/swagger_server/controllers/default_controller.py
@@ -0,0 +1,186 @@
+import connexion
+import six
+
+from swagger_server.models.book_room_id_body import BookRoomIdBody  # noqa: E501
+from swagger_server.models.inline_response200 import InlineResponse200  # noqa: E501
+from swagger_server.models.inline_response2001 import InlineResponse2001  # noqa: E501
+from swagger_server.models.inline_response400 import InlineResponse400  # noqa: E501
+from swagger_server.models.inline_response409 import InlineResponse409  # noqa: E501
+from swagger_server.models.inline_response500 import InlineResponse500  # noqa: E501
+from swagger_server.models.room_air_quality import RoomAirQuality  # noqa: E501
+from swagger_server.models.room_data import RoomData  # noqa: E501
+from swagger_server.models.room_humidity import RoomHumidity  # noqa: E501
+from swagger_server.models.room_light import RoomLight  # noqa: E501
+from swagger_server.models.room_temperature import RoomTemperature  # noqa: E501
+from swagger_server import util
+
+
+def book_room_id_post(body, room_id):  # noqa: E501
+    """Book a specific room
+
+    Book a 30-minute time slot for a specific room. # noqa: E501
+
+    :param body: 
+    :type body: dict | bytes
+    :param room_id: Unique identifier for the room
+    :type room_id: str
+
+    :rtype: InlineResponse200
+    """
+    if connexion.request.is_json:
+        body = BookRoomIdBody.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def room_id_airquality_get(room_id):  # noqa: E501
+    """Get air quality data for a specific room
+
+    Retrieve air quality measurements for a specific room # noqa: E501
+
+    :param room_id: Unique identifier for the room
+    :type room_id: str
+
+    :rtype: RoomAirQuality
+    """
+    return 'do some magic!'
+
+
+def room_id_bookings_get(room_id, start_date=None, days=None):  # noqa: E501
+    """Get bookings for specific room
+
+    Retrieve booking timestamps for a specific room within a specified time interval # noqa: E501
+
+    :param room_id: Unique identifier for the room
+    :type room_id: str
+    :param start_date: Start date for the booking period (YYYY-MM-DD)
+    :type start_date: str
+    :param days: Number of days to retrieve bookings for
+    :type days: int
+
+    :rtype: InlineResponse2001
+    """
+    start_date = util.deserialize_date(start_date)
+    return 'do some magic!'
+
+
+def room_id_humidity_get(room_id):  # noqa: E501
+    """Get humidity data for a specific room
+
+    Retrieve humidity measurements for a specific room # noqa: E501
+
+    :param room_id: Unique identifier for the room
+    :type room_id: str
+
+    :rtype: RoomHumidity
+    """
+    return 'do some magic!'
+
+
+def room_id_light_get(room_id):  # noqa: E501
+    """Get light data for a specific room
+
+    Retrieve light measurements for a specific room # noqa: E501
+
+    :param room_id: Unique identifier for the room
+    :type room_id: str
+
+    :rtype: RoomLight
+    """
+    return 'do some magic!'
+
+
+def room_id_temperature_get(room_id):  # noqa: E501
+    """Get temperature data for a specific room
+
+    Retrieve temperature measurements for a specific room # noqa: E501
+
+    :param room_id: Unique identifier for the room
+    :type room_id: str
+
+    :rtype: RoomTemperature
+    """
+    return 'do some magic!'
+
+
+def rooms_airquality_get():  # noqa: E501
+    """Get air quality data for all rooms
+
+    Retrieve air quality measurements for all rooms # noqa: E501
+
+
+    :rtype: List[RoomAirQuality]
+    """
+    return 'do some magic!'
+
+
+def rooms_bookings_get(start_date=None, days=None):  # noqa: E501
+    """Get bookings for all rooms
+
+    Retrieve booking timestamps for all rooms within a specified time interval # noqa: E501
+
+    :param start_date: Start date for the booking period (YYYY-MM-DD)
+    :type start_date: str
+    :param days: Number of days to retrieve bookings for
+    :type days: int
+
+    :rtype: Dict[str, List[Booking]]
+    """
+    start_date = util.deserialize_date(start_date)
+    return 'do some magic!'
+
+
+def rooms_humidity_get():  # noqa: E501
+    """Get humidity data for all rooms
+
+    Retrieve humidity measurements for all rooms # noqa: E501
+
+
+    :rtype: List[RoomHumidity]
+    """
+    return 'do some magic!'
+
+
+def rooms_light_get():  # noqa: E501
+    """Get light data for all rooms
+
+    Retrieve light measurements for all rooms # noqa: E501
+
+
+    :rtype: List[RoomLight]
+    """
+    return 'do some magic!'
+
+
+def rooms_temperature_get():  # noqa: E501
+    """Get temperature data for all rooms
+
+    Retrieve temperature measurements for all rooms # noqa: E501
+
+
+    :rtype: List[RoomTemperature]
+    """
+    return 'do some magic!'
+
+
+def sensor_room_id_get(room_id):  # noqa: E501
+    """Get sensor data for a specific room
+
+    Retrieve all sensor data for a specific room # noqa: E501
+
+    :param room_id: Unique identifier for the room
+    :type room_id: str
+
+    :rtype: RoomData
+    """
+    return 'do some magic!'
+
+
+def sensor_rooms_get():  # noqa: E501
+    """Get sensor data for all rooms
+
+    Retrieve all sensor data (temperature and air quality) for all rooms # noqa: E501
+
+
+    :rtype: List[RoomData]
+    """
+    return 'do some magic!'
