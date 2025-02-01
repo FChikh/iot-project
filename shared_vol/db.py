83fb2a53314742b9bd3cd774140d0a5bdcd923db
TarikTornes <tarik.tornes03@gmail.com>
2025-02-01 23:40:23 +0100

    Rewritten endpoint for equipment retrieval

--- shared_vol/db.py
+++ shared_vol/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 import os
 
 # Read environment variables from Docker Compose
-DB_NAME = os.getenv("POSTGRES_DB", "mydatabase")
+DB_NAME = os.getenv("POSTGRES_DB", "rooms_db")
 DB_USER = os.getenv("POSTGRES_USER", "user")
 DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
 DB_HOST = os.getenv("POSTGRES_HOST", "postgres")  # Use Docker service name
