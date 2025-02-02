504faea412ce46db4b025d1298cfa4db54e03914
TarikTornes <tarik.tornes03@gmail.com>
2025-02-02 01:08:11 +0100

    Ficed bug with additional_contexts

--- restapi_rooms/swagger_server/controllers/get_funcs/get_equipment.py
+++ restapi_rooms/swagger_server/controllers/get_funcs/get_equipment.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from sqlalchemy.orm import sessionmaker
-from shared_vol.db import engine  # Assuming you have a db.py file for engine setup
-from shared_vol.models import Room, Equipment  # Import the new models
+from ..db import engine  # Assuming you have a db.py file for engine setup
+from ..models import Room, Equipment  # Import the new models
 
 # Create a session factory
 Session = sessionmaker(bind=engine)
